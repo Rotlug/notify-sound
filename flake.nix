52fb05d7a3d6dc78c8d68ac5ea7186825e6603bb
rotlug <rotlug77@gmail.com>
2026-02-21 23:16:57 +0200

    chore: make flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              rustlings
+              clippy
+
+              alsa-lib
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
+          };
+        }
+    );
+}
